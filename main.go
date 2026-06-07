6b10677ebdfaadd59afcbbea56f3a406dce8e2ab
ChrisPC <moreirachristopher100@gmail.com>
2026-06-07 04:40:48 -0300

    feat: schema and custom logger

--- main.go
+++ main.go
@@ -1,10 +1,23 @@
 package main
 
-import "github.com/Christopher-Moreira/golang_apiREST/router"
+import (
+	"github.com/Christopher-Moreira/golang_apiREST/config"
+	"github.com/Christopher-Moreira/golang_apiREST/router"
+)
+
+var (
+	logger *config.Logger
+)
 
 func main() {
 
+	logger = config.GetLogger("main")
+	// Initialize Configs
+	err := config.Init()
+	if err != nil {
+		logger.Errorf("Config Initialization Error: %v", err)
+		return
+	}
 	// Initialize Router
-
 	router.Initialize()
 }
